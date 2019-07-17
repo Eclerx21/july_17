0a5c3c26df51810d4cbc1d5608aaf76264c6fbda
Raman <ramanpreetkaur.kaur9@gmail.com>
2019-07-17 11:24:13 -0400

    final commit

--- july_17/july_17/Program.cs
+++ july_17/july_17/Program.cs
@@ -4,10 +4,33 @@ namespace july_17
 {
     class Program
     {
+        static void Main(string[] args)
+        {
+            Console.WriteLine(" ");
+        }
+            
+    }
         class Node
         {
-            Node nextlink;
+            Node nextLink;
+            Node prevLink;
+            String nodeName;
+            internal Node nextLink;
 
         }
+    class LinkedList
+    {
+        Node Head, Orange, Tangerine, Tail;
+        public void CreateLinkedList()
+        {
+            Head = new Node();
+            Orange = new Node();
+            Tangerine = new Node();
+            Tail = Tangerine;
+            Head.nextLink =  Orange;
+            Console.WriteLine("yeahh!!");
+        }
     }
+
 }
+    
